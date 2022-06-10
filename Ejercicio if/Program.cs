4828b57a7586da4f6070fd5e16cdf2844ab008e8
Alexsaori1 <alexelleon2018@gmail.com>
2022-06-10 02:13:49 +0200

    Otro mas

---
+++ Ejercicio if/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            String dato = "";
+            Console.WriteLine("Por favor ingrese su edad:");
+            dato = Console.ReadLine();
+            numero1 = Convert.ToInt32(dato);
+            Console.WriteLine("Su edad es:"+ numero1);
+
+            Console.ReadLine();
+        }
+    }
+}
