2bed570d47cc48829e99683720184c52c1489ca5
Alexsaori1 <alexelleon2018@gmail.com>
2022-06-10 02:10:55 +0200

    Espero que funcione

---
+++ EjercicioEscritoProgramacion/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EjercicioEscritoProgramacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.Gold;
+            this.btnAceptar.Font = new System.Drawing.Font("Rockwell", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btnAceptar.Location = new System.Drawing.Point(65, 140);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(125, 37);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(30, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese su numero:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(196, 48);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(97, 20);
+            this.txtNumero1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(72, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Su numero es:";
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(196, 93);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(238, 20);
+            this.txtRespuesta.TabIndex = 4;
+            this.txtRespuesta.TextChanged += new System.EventHandler(this.txtRespuesta_TextChanged);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.Gold;
+            this.btnBorrar.Font = new System.Drawing.Font("Rockwell", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnBorrar.Location = new System.Drawing.Point(65, 201);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(125, 37);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(446, 450);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ EjercicioEscritoProgramacion/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioEscritoProgramacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Borrar()
+        {
+            txtNumero1.Clear();
+            txtRespuesta.Clear();
+
+        }
+        private void Mostrar(String numero)
+        {
+            String numero1 = txtNumero1.Text;
+            numero = numero1;
+            double numero2 = 0.0;
+
+            if (double.TryParse(numero, out numero2)) {
+
+                switch (numero2)
+                {
+
+                    case 0:
+
+                        txtRespuesta.Text = "Cero";
+
+
+                        break;
+
+
+                    case 1:
+
+                        txtRespuesta.Text = "Uno";
+
+
+                        break;
+
+                    case 2:
+
+                        txtRespuesta.Text = "Dos";
+
+
+                        break;
+
+                    case 3:
+
+                        txtRespuesta.Text = "Tres";
+
+
+                        break;
+
+                    case 4:
+
+                        txtRespuesta.Text = "Cuatro";
+
+
+                        break;
+
+                    case 5:
+
+                        txtRespuesta.Text = "Cinco";
+
+
+                        break;
+
+                    case 6:
+
+                        txtRespuesta.Text = "Seis";
+
+
+                        break;
+
+                    case 7:
+
+                        txtRespuesta.Text = "Siete";
+
+
+                        break;
+
+                    case 8:
+
+                        txtRespuesta.Text = "Ocho";
+
+
+                        break;
+
+                    case 9:
+
+                        txtRespuesta.Text = "Nueve";
+
+
+                        break;
+
+
+                    default:
+
+                        txtRespuesta.Text = "Error, por favor ingrese un numero entre 0 y 9";
+
+
+                        break;
+
+
+                }
+            } else {
+
+                MessageBox.Show("Error, solo puede ingresar numeros enteros del 0 al 9");
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            Mostrar(txtNumero1.Text);
+        }
+
+        private void txtRespuesta_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            Borrar();
+        }
+    }
+}
